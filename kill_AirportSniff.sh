20db9d293b114c12d789131be182b2ebc3c93be8
diegoesqui <diegoesqui@msn.com>
2015-04-04 18:38:10 +0200

    Rename kill_AirportSniff.sh as clean.sh

--- kill_AirportSniff.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Captura el pid de los procesos "Airport" y los mata
-#
-
-echo Stopping Airport
-ps -ef|grep "airport en1 sniff 1"|grep -v grep|awk '{system("sudo kill -9 " $2)}'
-
